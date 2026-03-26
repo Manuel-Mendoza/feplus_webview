a8d45546ded21059619a38caec05013d7d4050f5
Jose Manuel Campos Mendoza <frontenddesing.manuel@gmail.com>
2026-03-26 16:15:42 -0400

    chore: update app icons and banners across all drawable densities
    Replaced all app icon and banner assets with updated versions across all drawable density folders (drawable, drawable-xhdpi, drawable-xxxhdpi) to refresh the application's visual branding.

---
+++ app/build/generated/source/buildConfig/full/debug/top/rootu/lampa/BuildConfig.java
@@ -0,0 +1,17 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package top.rootu.lampa;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "top.rootu.lampa";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "full";
+  public static final int VERSION_CODE = 556;
+  public static final String VERSION_NAME = "1.0.0-4-g838c213-dirty";
+  // Field from product flavor: full
+  public static final String defaultAppUrl = "https://stfeplus.pages.dev";
+  // Field from product flavor: full
+  public static final boolean enableUpdate = false;
+}
