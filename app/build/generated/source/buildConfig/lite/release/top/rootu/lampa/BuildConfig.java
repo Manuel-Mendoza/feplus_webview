38d57b3f16e93cf54e53f0b93da4a974d662bc67
Jose Manuel Campos Mendoza <frontenddesing.manuel@gmail.com>
2026-03-30 20:49:08 -0400

    chore: bump version to 1.0.0-12 and update build artifacts
    - Increment VERSION_CODE to 564 and VERSION_NAME to 1.0.0-12-ga7377a2-dirty
    - Update generated build configuration and intermediate build files
    - Refresh timestamps and resource mappings for liteRelease build

--- app/build/generated/source/buildConfig/lite/release/top/rootu/lampa/BuildConfig.java
+++ app/build/generated/source/buildConfig/lite/release/top/rootu/lampa/BuildConfig.java
@@ -8,8 +8,8 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "top.rootu.feplus";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "lite";
-  public static final int VERSION_CODE = 563;
-  public static final String VERSION_NAME = "1.0.0-11-gde60c76";
+  public static final int VERSION_CODE = 564;
+  public static final String VERSION_NAME = "1.0.0-12-ga7377a2-dirty";
   // Field from product flavor: lite
   public static final String defaultAppUrl = "https://stfeplus.pages.dev";
   // Field from product flavor: lite
