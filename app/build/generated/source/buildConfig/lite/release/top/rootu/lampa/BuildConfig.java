bdb0330c9be0f238ca790993b546e3c34c711fc6
Jose Manuel Campos Mendoza <frontenddesing.manuel@gmail.com>
2026-04-05 23:05:52 -0400

    ci: bump version code to 568 and update version name
    The version code has been incremented from 567 to 568 and the version name updated from "1.0.0-15-g29d31b1-dirty" to "1.0.0-16-gaaa2745-dirty". This reflects the latest commit (aaa2745) and prepares the build for release.

--- app/build/generated/source/buildConfig/lite/release/top/rootu/lampa/BuildConfig.java
+++ app/build/generated/source/buildConfig/lite/release/top/rootu/lampa/BuildConfig.java
@@ -8,8 +8,8 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "top.rootu.feplus";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "lite";
-  public static final int VERSION_CODE = 567;
-  public static final String VERSION_NAME = "1.0.0-15-g29d31b1-dirty";
+  public static final int VERSION_CODE = 568;
+  public static final String VERSION_NAME = "1.0.0-16-gaaa2745-dirty";
   // Field from product flavor: lite
   public static final String defaultAppUrl = "http://hpgbmyiart.carmelocampos.com";
   // Field from product flavor: lite
